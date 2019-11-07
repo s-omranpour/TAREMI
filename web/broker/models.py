7b1a6ced9c00eed677d4c420222c650649f60360
ce441-981-95105721 <soroush.333@gmail.com>
2019-11-07 17:53:49 +0330

    redesign core models

--- web/broker/models.py
+++ web/broker/models.py
@@ -1,33 +1,44 @@
 from django.db import models
 
 from broker.constants import *
-from authentication.models import Instructor
+from authentication.models import Instructor, Student
 
 
-class Application(models.Model):
-    name = models.CharField("Name", max_length=NAME_LENGTH)
+class ApplicationForm(models.Model):
+    course_id = models.CharField("course_id", max_length=10)
     creator = models.ForeingKey(Instructor, on_delete=models.CASCADE)
     release_date = models.DateField("release_date", auto_now=True)
     deadline = models.DateField("deadline", auto_now=True)
+    info = models.CharField("information", max_length=1000)
 
-
-class MultiChoiceQuestion(models.Model):
+class Question(models.Model):
     form = models.ForeignKey(Application, on_delete=models.CASCADE, related_name="textual_questions")
-    question = models.CharField("Question", max_length=QUESTION_MAX_LENGTH)
+    question = models.CharField("question", max_length=QUESTION_MAX_LENGTH)
 
 
-class TextualQuestion(models.Model):
-    form = models.ForeignKey(Application, on_delete=models.CASCADE, related_name="textual_questions")
-    question = models.CharField("Question", max_length=QUESTION_MAX_LENGTH)
-    answer = models.CharField("Answer", max_length=ANSWER_MAX_LENGTH)
+class ApplicationResponse(models.Model):
+    owner = models.ForeignKey(Student, on_delete=models.CASCADE)
+    date = models.DateField("date_submitted", auto_now=True)
+
+class Answer(models.Model):
+    class Meta:
+        question_class = None
+
+    response = models.ForeignKey(StudentResponse, on_delete=models.CASCADE)
+    question = models.ForeignKey(_meta.question_class, on_delete=models.CASCADE)
+
+class TextualQuestion(Question):
+    pass
+class TextAnswer(Answer):
+    value = models.CharField('text_value', max_length=100)
+
+class MultiChoiceQuestion(Question):
+    pass
+class MultiChoiceAnswer(Answer):
+    value = models.CharField("choice_value", max_length=CHOICE_MAX_LENGTH)
 
 
-class NumericalQuestion(models.Model):
-    form = models.ForeignKey(Application, on_delete=models.CASCADE, related_name="textual_questions")
-    question = models.CharField("Question", max_length=QUESTION_MAX_LENGTH)
-    answer = models.IntegerField("Answer", max_length=ANSWER_MAX_LENGTH)
-
-
-class Choice(models.Model):
-    value = models.CharField("Value", max_length=CHOICE_MAX_LENGTH)
-    question = models.ForeignKey(MultiChoiceQuestion, on_delete=models.CASCADE)
+class NumericalQuestion(Question):
+    pass
+class NumericalAnswer(Answer):
+    value = models.IntegerField('int_value')
