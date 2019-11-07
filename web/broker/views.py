d07fae593b50242a9de876f6e30950ca172bc923
ce441-981-95105721 <soroush.333@gmail.com>
2019-11-08 02:47:37 +0330

    instructor pov. pending for create new form and student pov

--- web/broker/views.py
+++ web/broker/views.py
@@ -1,5 +1,6 @@
 
 from django.shortcuts import render, redirect
+from django.db.models.query import EmptyQuerySet
 
 from .models import *
 from django.http import HttpResponse
@@ -7,12 +8,11 @@ from django.contrib.auth.decorators import login_required
 
 def home(request):
     user = request.user
-    if user.student:
-        return redirect('student_home')
-    return
-
-def instructor_home(instructor):
-    pass
+    try:
+        if user.student:
+            return redirect('student_home')
+    except:
+        return redirect('instructor_home')
 
 
 def student_home(student):
@@ -20,7 +20,37 @@ def student_home(student):
 
 @login_required(login_url='/account/login')
 def instructor_home(request):
-    return render(request, 'broker/instructor_home.html', context={})
+    user = request.user
+    forms = user.instructor.forms.all()
+    responses = [len([answer.response for answer in form.questions.first().answers.all()]) for form in forms]
+    return render(request, 'broker/instructor_home.html', context={'user':user, 'form_res':zip(forms,responses)})
+
+
+@login_required(login_url='/account/login')
+def instructor_form_detail(request, id):
+    user = request.user
+    form = ApplicationForm.objects.filter(id=id).first()
+    if isinstance(form, EmptyQuerySet):
+        # todo: error
+        pass
+    responses = [answer.response for answer in form.questions.first().answers.all()]
+    constext = {'form':form , 'responses':responses}
+    return HttpResponse(form.info)
+
+
+@login_required(login_url='/account/login')
+def instructor_response_detail(request, id):
+    user = request.user
+    response = ApplicationResponse.objects.filter(id=id).first()
+    if isinstance(response, EmptyQuerySet):
+        # todo: error
+        pass
+    return HttpResponse(response)
+
+
+@login_required(login_url='/account/login')
+def instructor_create_form(request):
+    return 
 
 def form_filling(request):
     return render(request, 'broker')
