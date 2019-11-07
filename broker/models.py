8ac55487d43a5a2f4b3fdd9e7d7ed8d66fcdd7bd
ce441-981-95103729 <tigerous215@gmail.com>
2019-11-07 14:21:20 +0330

    ui initiated

--- broker/models.py
+++ broker/models.py
@@ -3,23 +3,28 @@ from django.db import models
 from broker.constants import *
 from authentication.models import Instructor
 
+
 class ApplicationForm(models.Model):
     name = models.CharField("Name", max_length=NAME_LENGTH)
     creator = models.ForeingKey(Instructor, on_delete=models.CASCADE)
 
+
 class MultiChoiceQuestion(models.Model):
     form = models.ForeignKey(ApplicationForm, on_delete=models.CASCADE, related_name="textual_questions")
     question = models.CharField("Question", max_length=QUESTION_MAX_LENGTH)
+
 
 class TextualQuestion(models.Model):
     form = models.ForeignKey(ApplicationForm, on_delete=models.CASCADE, related_name="textual_questions")
     question = models.CharField("Question", max_length=QUESTION_MAX_LENGTH)
     answer = models.CharField("Answer", max_length=ANSWER_MAX_LENGTH)
 
+
 class NumericalQuestion(models.Model):
     form = models.ForeignKey(ApplicationForm, on_delete=models.CASCADE, related_name="textual_questions")
     question = models.CharField("Question", max_length=QUESTION_MAX_LENGTH)
     answer = models.IntegerField("Answer", max_length=ANSWER_MAX_LENGTH)
+
 
 class Choice(models.Model):
     value = models.CharField("Value", max_length=CHOICE_MAX_LENGTH)
--- taremi/settings.py
+++ taremi/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'taremi.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'templates'), os.path.join(BASE_DIR, 'broker/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
