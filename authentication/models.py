6531408e0827f16ec72971518164d7b1ddb88689
ce441-981-95105721 <soroush.333@gmail.com>
2019-11-07 10:32:42 +0330

    models synced with auth user

--- authentication/models.py
+++ authentication/models.py
@@ -1,15 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 class Student(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=50)
     surname = models.CharField(max_length=50)
-    student_id = models.CharField(max_length=20)
-    major = models.CharField(max_length=50)
-    enterance_year = models.CharField(max_length=4)
+    student_id = models.IntegerField(max_length=20)
+    major = models.(max_length=50)
+    enterance_year = models.IntegerField(max_length=4)
 
 
 class Instructor(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=50)
     surname = models.CharField(max_length=50)
 
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Student.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.student.save()
 
