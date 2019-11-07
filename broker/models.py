88651a522a363cc4126484138938fbd72a6a28a5
ce441-981-95103729 <tigerous215@gmail.com>
2019-11-07 16:29:59 +0330

    application models

--- broker/models.py
+++ broker/models.py
@@ -4,24 +4,26 @@ from broker.constants import *
 from authentication.models import Instructor
 
 
-class ApplicationForm(models.Model):
+class Application(models.Model):
     name = models.CharField("Name", max_length=NAME_LENGTH)
     creator = models.ForeingKey(Instructor, on_delete=models.CASCADE)
+    release_date = models.DateField("release_date", auto_now=True)
+    deadline = models.DateField("deadline", auto_now=True)
 
 
 class MultiChoiceQuestion(models.Model):
-    form = models.ForeignKey(ApplicationForm, on_delete=models.CASCADE, related_name="textual_questions")
+    form = models.ForeignKey(Application, on_delete=models.CASCADE, related_name="textual_questions")
     question = models.CharField("Question", max_length=QUESTION_MAX_LENGTH)
 
 
 class TextualQuestion(models.Model):
-    form = models.ForeignKey(ApplicationForm, on_delete=models.CASCADE, related_name="textual_questions")
+    form = models.ForeignKey(Application, on_delete=models.CASCADE, related_name="textual_questions")
     question = models.CharField("Question", max_length=QUESTION_MAX_LENGTH)
     answer = models.CharField("Answer", max_length=ANSWER_MAX_LENGTH)
 
 
 class NumericalQuestion(models.Model):
-    form = models.ForeignKey(ApplicationForm, on_delete=models.CASCADE, related_name="textual_questions")
+    form = models.ForeignKey(Application, on_delete=models.CASCADE, related_name="textual_questions")
     question = models.CharField("Question", max_length=QUESTION_MAX_LENGTH)
     answer = models.IntegerField("Answer", max_length=ANSWER_MAX_LENGTH)
 
