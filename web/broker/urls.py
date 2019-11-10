82f28096759fb0cf79e74d98342bbeceae4ebc32
Sajad <sska1377@gmail.com>
2019-11-10 20:00:27 +0330

    O, finally

--- web/broker/urls.py
+++ web/broker/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('student/', views.student_home, name='student_home'),
     path('apply/<int:id>/', views.application, name='apply_form'),
+    path('apply/success/', views.application_success, name='application_success'),
     path('instructor/', views.instructor_home, name='instructor_home'),
     path('instructor/form/<int:id>/', views.instructor_form_detail, name='form_detail'),
     path('instructor/res/<int:id>/', views.instructor_response_detail, name='res_detail'),
