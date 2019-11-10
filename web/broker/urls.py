40f75e9b3b8b1793e7c37f56b2098adcaf1ab0cf
Sajad <sska1377@gmail.com>
2019-11-11 01:27:09 +0330

    its over, its finally over

--- web/broker/urls.py
+++ web/broker/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('apply/<int:id>/', views.application, name='apply_form'),
     path('apply/success/', views.application_success, name='application_success'),
     path('instructor/', views.instructor_home, name='instructor_home'),
+    path('instructor/form/new/', views.instructor_create_form, name='create_form'),
     path('instructor/form/<int:id>/', views.instructor_form_detail, name='form_detail'),
     path('instructor/res/<int:id>/', views.instructor_response_detail, name='res_detail'),
 ]
--- web/broker/views/instructor.py
+++ web/broker/views/instructor.py
@@ -1,17 +1,20 @@
+from datetime import timedelta
+
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.views.decorators.csrf import csrf_exempt
 
 from ..models import *
 
-@login_required(login_url='/account/login')
+@login_required()
 def instructor_home(request):
     user = request.user
     forms = user.instructor.forms.all()
     return render(request, 'broker/instructor/home.html', context={'user':user, 'forms':forms })
 
 
-@login_required(login_url='/account/login')
+@login_required()
 def instructor_form_detail(request, id):
     user = request.user
     form = ApplicationForm.objects.filter(id=id).first()
@@ -23,7 +26,7 @@ def instructor_form_detail(request, id):
     return HttpResponse(form.info)
 
 
-@login_required(login_url='/account/login')
+@login_required()
 def instructor_response_detail(request, id):
     user = request.user
     response = ApplicationResponse.objects.filter(id=id).first()
@@ -32,7 +35,21 @@ def instructor_response_detail(request, id):
         pass
     return HttpResponse(response)
 
-
-@login_required(login_url='/account/login')
+# TODO move this shit to API
+@csrf_exempt
+@login_required()
 def instructor_create_form(request):
-    return
+    if request.method == "GET":
+        return render(request, 'broker/instructor/form_creation.html', {})
+    else:
+        form = ApplicationForm(creator=request.user.instructor)
+        form.deadline = form.release_date + timedelta(days=10)
+        form.course_id = request.POST["course"]
+        form.info = request.POST["info"]
+        form.save()
+        for i in range(1, int(reqest.POST["length"]) + 1):
+            if request.POST["q_%d_type" % i] == "textual":
+                q = TextualQuestion(form=form, question=request.POST["q_%d_body" % i], number=i)
+                q.save()
+
+        return HttpResponse();
